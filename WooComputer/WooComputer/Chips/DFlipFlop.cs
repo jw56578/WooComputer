174fe913b4138bfc1b34535c3b188dec3df71340
jw56578 <jw56578@gmail.com>
2016-03-08 16:02:28 -0600

    comments

--- WooComputer/WooComputer/Chips/DFlipFlop.cs
+++ WooComputer/WooComputer/Chips/DFlipFlop.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace WooComputer.Chips
 {
+    //this is like the Nand gate, maintaining a C# field represents the concept that we don't know how a flip flop really works because its a 
+    //electrical engineering thing
     public class DFlipFlop
     {
         public bool Value {get;set;}
